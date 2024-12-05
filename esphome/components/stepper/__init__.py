86c08ec2850c9a572a6832844505a8140d4fed51
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2024-12-05 14:30:23 +1300

    Update esphome/components/stepper/__init__.py

--- esphome/components/stepper/__init__.py
+++ esphome/components/stepper/__init__.py
@@ -92,8 +92,7 @@ async def setup_stepper_core_(stepper_var, config):
         cg.add(stepper_var.set_deceleration(config[CONF_DECELERATION]))
     if CONF_MAX_SPEED in config:
         cg.add(stepper_var.set_max_speed(config[CONF_MAX_SPEED]))
-    if CONF_ROTATION in config:
-        cg.add(stepper_var.set_rotation(config[CONF_ROTATION]))
+    cg.add(stepper_var.set_rotation(config[CONF_ROTATION]))
 
 
 async def register_stepper(var, config):
